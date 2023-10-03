5e279439165db32175dc54cf6f4e0b0dc5d89e5a
emarkees <rphillipse@gmail.com>
2023-10-03 16:27:27 -0600

    Add javascript file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  namespace :api do
+    get 'random_greeting', to: 'greetings#random_greeting'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
 end
