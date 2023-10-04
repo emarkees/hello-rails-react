57871c20e655fc88d0504a261de5afa217888c28
emarkees <rphillipse@gmail.com>
2023-10-04 17:32:19 -0600

    Greetings Component added

--- app/javascript/component/app.js
+++ app/javascript/component/app.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Greetings from "./greetings";
 
 function App() {
   return (
---
+++ app/javascript/component/greetings.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Greetings () {
+  return (
+    <h1>greeevve</h1>
+  );
+};
+
+export default Greetings;
