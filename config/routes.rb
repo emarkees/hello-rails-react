6058259fe3ece467c5e276b9077b2f7a60756cf7
emarkees <rphillipse@gmail.com>
2023-10-04 07:11:05 -0600

    Add api and welcom files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  namespace :api do
+    get 'greetings/random_greeting'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
